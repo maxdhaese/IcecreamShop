4367c22fae63cbb09d935854e2d0aeca786fa60b
maxdhaese <maxdhaese@gmail.com>
2019-05-16 14:46:23 +0200

    commit

--- src/be/intecbrussel/sellers/IceCreamSalon.java
+++ src/be/intecbrussel/sellers/IceCreamSalon.java
@@ -1,4 +1,35 @@
 package be.intecbrussel.sellers;
 
-public class IceCreamSalon {
+import be.intecbrussel.eatables.Cone;
+import be.intecbrussel.eatables.IceRocket;
+import be.intecbrussel.eatables.Magnum;
+
+public class IceCreamSalon implements IceCreamSeller {
+
+    private PriceList priceList;
+    private double totalProfit;
+
+    public IceCreamSalon(PriceList priceList) {
+        this.priceList = priceList;
+    }
+
+    @Override
+    public Cone orderCone(Cone.Flavor[] flavors) {
+        return null;
+    }
+
+    @Override
+    public IceRocket orderIceRocket() {
+        return null;
+    }
+
+    @Override
+    public Magnum orderMagnum(Magnum.MagnumType type) {
+        return null;
+    }
+
+    @Override
+    public double getProfit() {
+        return totalProfit;
+    }
 }
