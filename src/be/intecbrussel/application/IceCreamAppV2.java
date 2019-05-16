740d6e56a4db7740f018453f48a0f22b1a4aa086
maxdhaese <maxdhaese@gmail.com>
2019-05-16 23:35:44 +0200

    commit

--- src/be/intecbrussel/application/IceCreamAppV2.java
+++ src/be/intecbrussel/application/IceCreamAppV2.java
@@ -1,6 +1,8 @@
 package be.intecbrussel.application;
 
 import be.intecbrussel.eatables.Cone;
+import be.intecbrussel.eatables.Eatable;
+import be.intecbrussel.eatables.Magnum;
 import be.intecbrussel.sellers.IceCreamCar;
 import be.intecbrussel.sellers.NoMoreIceCreamException;
 import be.intecbrussel.sellers.PriceList;
@@ -10,13 +12,28 @@ public class IceCreamAppV2 {
     public static void main(String[] args) {
 
         PriceList priceList = new PriceList(0.5,1.5,2);
-        Stock stock = new Stock(0,0,0,0);
+        Stock stock = new Stock(1,3,4,5);
         IceCreamCar iceCreamCar = new IceCreamCar(priceList,stock);
 
+
         try {
-            iceCreamCar.orderCone(new Cone.Flavor[]{Cone.Flavor.STRAWBERRY});
+            Eatable[] eatables = {
+                    iceCreamCar.orderMagnum(Magnum.MagnumType.BLACKCHOCOLATE),
+                    iceCreamCar.orderIceRocket(),
+                    iceCreamCar.orderIceRocket(),
+                    iceCreamCar.orderCone(new Cone.Flavor[]{Cone.Flavor.PISTACHE, Cone.Flavor.STRAWBERRY})};
+            for (Eatable iceSalon: eatables)
+                iceSalon.eat();
+            
         } catch (NoMoreIceCreamException e) {
             e.printStackTrace();
+        }finally {
+            System.out.println("The profit is : € " + iceCreamCar.getProfit());
         }
+
+
+
+
+
     }
 }
